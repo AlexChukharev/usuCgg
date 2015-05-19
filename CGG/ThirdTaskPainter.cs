e09e3b40ac5207850762439f633c1f2d79387777
= <=>
2015-05-19 13:30:09 +0500

    delete some coments

--- CGG/ThirdTaskPainter.cs
+++ CGG/ThirdTaskPainter.cs
@@ -80,50 +80,5 @@ namespace CGG
 			};
 			canvas.Children.Add(line);
 		}
-
-		/*
-			int k;
-            int h = (int)window1.ActualHeight - 50;
-            int w = (int)window1.ActualWidth - 50;
-            if (a > b) 
-                k = (int)(w / a);
-            else 
-                k = (int)(h / b);
-            int XRadius = (int)a*k/2;
-            int YRadius = (int)b*k/2;
-    
-            int xr2 = XRadius*XRadius*2; 
-            int yr2 = YRadius*YRadius*2;
-            int x = 0; 
-            int y = YRadius; 
-            int d = (yr2 - xr2 * y) + xr2/2;
-            double xm = xr2 / Math.Sqrt((xr2 + yr2)*2) - 1;
-            draw4Pixel(w/2, h/2, x, y);
-            while (x < xm) {
-                if (d > 0) {
-                    y--;
-                    d = d + yr2 * ((x*2) + 3) - xr2 * y * 2;
-                }
-                else
-                    d = d + yr2 * ((x*2) + 3);
-                x++;
-                draw4Pixel(w / 2, h / 2, x, y);
-            }
-            d = (xr2 - yr2 * XRadius) + yr2/2; 
-            x = XRadius; 
-            y = 0;
-            draw4Pixel(w / 2, h / 2, x, y);
-            xm = xm + 2;
-            while (x > xm) {
-            if (d > 0) {
-                x--;
-                d = d + xr2 * (y*2 + 3) - yr2 * x * 2;
-            }
-            else
-                d = d + xr2 * (y*2 + 3);
-            y++;
-            draw4Pixel(w / 2, h / 2, x, y);
-            }
-		 */
 	}
 }
